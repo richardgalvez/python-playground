8fc084e2a9df8fb8998cd5995a0c260a40974672
Richard Galvez <richardalbertogalvez@gmail.com>
2025-04-09 23:36:52 -0400

    simple-calculator complete.

--- simple-calculator/main.py
+++ simple-calculator/main.py
@@ -2,8 +2,8 @@
 
 # Simple Calculator
 
-# Create a simple, interactive CLI calculator that can add, subtract, multiply, and divide two provided numbers.
-# Implement check logic (input numbers only), keep asking until numbers are provided or user exits.
+# Create a simple, interactive CLI calculator that can add, subtract, multiply, and divide two positive integer inputs.
+# Implement detection for input to be numbers only, keep asking until numbers are provided or user exits, and account for divide by zero error.
 
 def simple_calculator():
   is_complete = False
@@ -15,20 +15,23 @@ def simple_calculator():
     if x == "":
       is_complete = True
     elif x.isnumeric() == False:
-      print("\nPlease enter only numbers.")
+      print("\nPlease enter only positive numbers.")
     else:
-      y = input("\nPlease enter the second number or leave empty for 0: ")
-      if y == "":
-        y = "0"
-        print("I am here")
-      # TODO: Fix detection by looping back to input option, cannot use else statement
-      elif y.isnumeric() == False:
-        print("\nPlease enter only numbers or leave empty for 0.")
-      
-      x = int(x)
-      y = int(y)
+      second_input = False
 
-      while is_calculated == False:
+      while second_input == False:  # Second input gets its own while loop for numeric detection.
+        y = input("\nPlease enter the second number or leave empty for 0: ")
+        if y == "":
+          y = "0"
+          second_input = True
+        elif y.isnumeric() == False:
+          print("\nPlease enter only positive numbers or leave empty for 0.")
+        else: 
+          second_input = True
+
+      while is_calculated == False:   # Once numbers are input after validation, it moves to calculation.
+        x = int(x)
+        y = int(y)
         operation = input("\nWhat mathematical operation do you want to use? Type in +, -, *, or /: ")
         add = x + y
         subtract = x - y
@@ -41,20 +44,20 @@ def simple_calculator():
           divide = int(x / y)
 
         if operation == "+":
-          print("\n" + str(add))
+          print("\n" + str(x) + " + " + str(y) + " = " + str(add))
           is_calculated = True
         elif operation == "-":
-          print("\n" + str(subtract))
+          print("\n" + str(x) + " - " + str(y) + " = " + str(subtract))
           is_calculated = True
         elif operation == "*":
-          print("\n" + str(muliply))
+          print("\n" + str(x) + " * " + str(y) + " = " + str(muliply))
           is_calculated = True
         elif operation == "/":
           if y == 0:
             print("\n" + "Undefined - Cannot divide by zero.")
             is_calculated = True
           else:  
-            print("\n" + str(divide))
+            print("\n" + str(x) + " / " + str(y) + " = " + str(divide))
             is_calculated = True
         else:
           print("\nInvalid operation, please enter again from below.")
