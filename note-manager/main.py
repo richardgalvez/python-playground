58ab458e3796735439d61dc6d5e4dee1cc6c9c2e
Richard Galvez <richardalbertogalvez@gmail.com>
2025-04-21 00:18:21 -0400

    note-manager - WIP to fix return type of get_note method.

--- note-manager/main.py
+++ note-manager/main.py
@@ -1,105 +1,104 @@
 from datetime import datetime
 import json
 
+
 class NoteManager:
+    def create_note(self, title: str, content: str) -> None:
+        """
+        Saves a new note with a title, content, and timestamp. Titles must be unique.
+        """
+        details = {
+            "title": title,
+            "content": content,
+            "timestamp": datetime.now().isoformat(),
+        }
+        note = json.dumps(details)
+        title_exists = False
 
-  def create_note(self, title: str, content: str) -> None:
-    """
-    Saves a new note with a title, content, and timestamp. Titles must be unique.
-    """
-    details = {
-        "title": title,
-        "content": content,
-        "timestamp": datetime.now().isoformat()
-    }
-    note = json.dumps(details)
-    title_exists = False
+        if len(title) <= 0 or len(title) <= 0:
+            raise ValueError("Title or Content provided cannot be empty.")
 
-    if len(title) <= 0 or len(title) <= 0:
-      raise ValueError("Title or Content provided cannot be empty.")
+        # Check for existing note based on inital JSON formatting + title, set flag to True if so.
+        file = open("notes.json")
+        for i in file:
+            if i[11 : 11 + len(title)] == title:
+                title_exists = True
+        file.close()
 
-    # Check for existing note based on inital JSON formatting + title, set flag to True if so.
-    file = open("notes.json")
-    for i in file:
-      if i[11:11 + len(title)] == title:
-        title_exists = True
-    file.close()
+        # Based on flag, raise error if already exists or proceed to create the new note.
+        if title_exists:
+            raise ValueError(
+                "This is the title of a note that already exists, please use a unique title name."
+            )
+        else:
+            file = open("notes.json", "a")
+            file.write(note)
+            file.write("\n")
+            file.close()
 
-    # Based on flag, raise error if already exists or proceed to create the new note.
-    if title_exists == True:
-      raise ValueError("This is the title of a note that already exists, please use a unique title name.")
-    else:
-      file = open("notes.json", "a")
-      file.write(note)
-      file.write("\n")
-      file.close()
+            print("New note added.")
 
-      print("New note added.")
+    def get_note(self, title: str) -> dict[str, str]:
+        """
+        Returns a note's details by its title.
+        """
+        desired_note = ""
 
+        file = open("notes.json")
+        for i in file:
+            if i[11 : 11 + len(title)] == title:
+                desired_note = i
+        file.close()
 
-  def get_note(self, title: str) -> dict:
-    """
-    Returns a note's details by its title.
-    """
-    desired_note = ""
+        if len(desired_note) == 0:
+            raise KeyError("Note cannot be found as it does not exist.")
+        elif len(desired_note) >= 1:
+            print(desired_note)
+            return desired_note
 
-    file = open("notes.json")
-    for i in file:
-      if i[11:11 + len(title)] == title:
-        desired_note = i
-    file.close()
+    def delete_note(self, title: str) -> None:
+        """
+        Removes a note from storage based on its title.
+        """
+        line_count = 0
+        found_title = ""
 
-    if len(desired_note) == 0:
-      raise KeyError("Note cannot be found as it does not exist.")
-    elif len(desired_note) >= 1:
-      print(desired_note)
-      return desired_note
+        # Search for note to be deleted based on title
+        infile = open("notes.json", "r").readlines()
+        for i in infile:
+            line_count += 1
+            if i[11 : 11 + len(title)] == title:
+                found_title = i[11 : 11 + len(title)]
+                break
 
+        # Detect if note exists, and if so, proceed to "delete" by overwriting notes.json file with all lines except the note that was specified by its title.
+        if found_title != title:
+            raise KeyError("Note cannot be deleted as it does not exist.")
+        else:
+            with open("notes.json", "w") as outfile:
+                for index, line in enumerate(infile):
+                    if index != line_count - 1:
+                        outfile.write(line)
 
-  def delete_note(self, title: str) -> None:
-    """
-    Removes a note from storage based on its title.
-    """
-    line_count = 0
-    found_title = ""
+            print("Note deleted.")
 
-    # Search for note to be deleted based on title
-    infile = open("notes.json", "r").readlines()
-    for i in infile:
-      line_count += 1
-      if i[11:11 + len(title)] == title:
-        found_title = i[11:11 + len(title)]
-        break
-    
-    # Detect if note exists, and if so, proceed to "delete" by overwriting notes.json file with all lines except the note that was specified by its title.
-    if (found_title != title):
-      raise KeyError("Note cannot be deleted as it does not exist.")
-    else:
-      with open("notes.json", "w") as outfile:
-        for index, line in enumerate(infile):
-          if index != line_count - 1:
-            outfile.write(line)
+    def list_notes(self) -> list[str]:
+        """
+        Lists the titles of all existing notes.
+        """
+        note_titles = []
 
-      print("Note deleted.")
+        # Load notes line by line from JSON format into dict format, pick out "title" key only from each, then append to final array for display.
+        file = open("notes.json")
+        for i in file:
+            line = i
+            line_dict = json.loads(line)
+            title_key = line_dict["title"]
+            note_titles.append(title_key)
+        file.close()
 
-
-  def list_notes(self) -> list[str]:
-    """
-    Lists the titles of all existing notes.
-    """
-    note_titles = []
-
-    # Load notes line by line from JSON format into dict format, pick out "title" key only from each, then append to final array for display.
-    file = open("notes.json")
-    for i in file:
-      line = i
-      line_dict = json.loads(line)
-      title_key = line_dict["title"]
-      note_titles.append(title_key)
-    file.close()
-
-    print(note_titles)
-    return note_titles
+        print(note_titles)
+        return note_titles
 
 
 ### Testing Section ###
