782da755227ad5e7da2d4240a6844056bc034179
Richard Galvez <richardalbertogalvez@gmail.com>
2025-05-08 13:02:36 -0400

    Trapper - database.py for DB connection created.

--- Trapper/server/db/database.py
+++ Trapper/server/db/database.py
@@ -1,2 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+# TODO: Establish connection to database
+url = URL.create(
+    drivername="postgresql",
+    username="tadmin",
+    password="troot",
+    host="postgres",
+    database="trapper-db",
+    port=5432,
+)
+
+engine = create_engine(url)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
---
+++ Trapper/server/db/models.py
@@ -0,0 +1,18 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+
+# TODO: Create db tables on startup
+
+# id: auto-increment
+
+# title: str (required)
+
+# description: str (required)
+
+# priority: str (low, medium, high)
+
+# status: str (open, resolved, default: open)
+
+# user_id: foreign key to User
+
+# created_at: datetime
