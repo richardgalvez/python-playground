4765e72d2cf265d5d2db5b42bd55615e24822eab
Richard Galvez <richardalbertogalvez@gmail.com>
2025-05-08 09:03:30 -0400

    Trapper - Set up issue management routes to be implemented.

--- Trapper/server/main.py
+++ Trapper/server/main.py
@@ -2,7 +2,31 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# TODO: User Authentication - 5 routes
+
+# TODO: Issue Management - All actions must be scoped to the current user
+
 
 @app.get("/")
 def home():
-    return {"message": "Hello world"}
+    return {"message": "Homepage with all issues for the logged-in user."}
+
+
+@app.get("/report")
+def new_issue():
+    return {"message": "New issue form."}
+
+
+@app.post("/report")
+def create_issue():
+    return {"message": "Create new issue for current user."}
+
+
+@app.get("/issues/{id}")
+def get_issues():
+    return {"message": "View issue detail page."}
+
+
+@app.post("/issues/{id}/resolve")
+def resolve_issue():
+    return {"message": "Mark an issue as resolved."}
