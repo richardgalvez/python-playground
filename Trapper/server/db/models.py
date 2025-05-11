af148ee65c323947fad7904ac4cdb2cc00baafb2
Richard Galvez <richardalbertogalvez@gmail.com>
2025-05-11 12:55:12 -0400

    Trapper - Converted credentials to use .env files.

--- Trapper/server/db/models.py
+++ Trapper/server/db/models.py
@@ -8,13 +8,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DB_DRIVER = os.getenv("DB_DRIVER", "")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 
 url = URL.create(
-    drivername="postgresql",
+    DB_DRIVER,
     username=DB_USER,
     password=DB_PASSWORD,
     host=DB_HOST,
