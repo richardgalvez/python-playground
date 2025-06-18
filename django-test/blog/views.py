19568bdf08401b655df949d62d73badd5384108f
Richard Galvez <richardalbertogalvez@gmail.com>
2025-06-17 22:03:57 -0400

    django-test - Display and code working.

--- django-test/blog/views.py
+++ django-test/blog/views.py
@@ -1,9 +1,8 @@
-from django.http import request
 from django.shortcuts import render
 from .models import Post
 
 
 # Create your views here.
-def home():
+def home(request):
     posts = Post.objects.all()
     return render(request, "blog/home.html", {"posts": posts})
